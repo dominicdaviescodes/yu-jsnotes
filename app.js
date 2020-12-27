cbe587a660c525dc699ab1a7f441262c0a7045be
dominicdaviescodes <codewithdom@gmail.com>
2020-12-27 20:54:41 +0000

    add notes to L106

---
+++ app.js
@@ -0,0 +1,31 @@
+    var a = '3';
+    var b = '8';
+
+    /***********Do not change the code above 👆*******/
+    //Write your code on lines 7 - 9:
+
+    // use another bucket 'c',
+
+    var c = a;
+    a = b;
+    b = c; 
+
+    /***********Do not change the code below 👇*******/
+
+    console.log('a is ' + a);
+    console.log('b is ' + b);
+
+
+
+
+
+/////////////////////////////////////
+// Lecture 103
+
+// need to commit this to memory
+// prompt('What is your name?');
+
+// use a variable
+// var yourName = prompt('What is your name?');
+
+// alert('Hello ' + yourName);
